9bf93e48958c3920ca5e2628a0fdaa12385ae6e9
jossanCosta <jocostasan@gmail.com>
2021-05-20 11:35:44 -0300

    refactor

--- processings/removePoints.py
+++ processings/removePoints.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-
+from qgis.PyQt.QtCore import QCoreApplication, QVariant
 from qgis.PyQt.QtCore import QCoreApplication
 from qgis.core import (
                         QgsProcessing,
@@ -26,7 +26,7 @@ class RemovePoints(QgsProcessingAlgorithm):
     INPUT_LAYERS_L = 'INPUT_LAYERS_L'
     INPUT_LAYERS_A = 'INPUT_LAYERS_A'
     INPUT_FRAME = 'INPUT_FRAME'
-    OUTPUT = 'OUTPUT'
+    OUTPUT_P = 'OUTPUT_P'
 
     def initAlgorithm(self, config=None):
         self.addParameter(
@@ -53,27 +53,60 @@ class RemovePoints(QgsProcessingAlgorithm):
             )
         )
 
+        self.addParameter(
+            QgsProcessingParameterFeatureSink(
+                self.OUTPUT_P,
+                self.tr('Flag')
+            )
+        ) 
+
     def processAlgorithm(self, parameters, context, feedback):      
         points = self.parameterAsVectorLayer(parameters, self.INPUT_FRAME, context)
-        for pointFeature in points.getFeatures():
-            for layerList in [ 
-                    self.parameterAsLayerList(parameters, self.INPUT_LAYERS_L, context), 
-                    self.parameterAsLayerList(parameters, self.INPUT_LAYERS_A, context)
-                ]:
-                self.deleteAllVerticesInLayers( pointFeature.geometry(), layerList )
-        return {self.OUTPUT: ''}
+        lines = self.parameterAsLayerList(parameters, self.INPUT_LAYERS_L, context), 
+        polygons = self.parameterAsLayerList(parameters, self.INPUT_LAYERS_A, context)
+        
+        CRS = QgsCoordinateReferenceSystem( iface.mapCanvas().mapSettings().destinationCrs().authid() )
+        fields = core.QgsFields()
+        fields.append(core.QgsField('erro', QVariant.String))
+        (sink_p, sinkId_p) = self.parameterAsSink(
+            parameters,
+            self.OUTPUT_P,
+            context,
+            fields,
+            core.QgsWkbTypes.MultiPoint,
+            CRS
+        )
 
-    def deleteAllVerticesInLayers(self, pointGeometry, layers):
-        for layer in layers:
-            for feature in layer.getFeatures():
-                featureGeometry = feature.geometry()
-                vertices = list( featureGeometry.vertices() )
-                for vertice in vertices:
-                    if not pointGeometry.intersects( core.QgsGeometry.fromPointXY( QgsPointXY(vertice) )   ):
-                        continue
-                    _, vertexAt = featureGeometry.closestVertexWithContext( QgsPointXY( vertice ) )
-                    featureGeometry.deleteVertex( vertexAt )
-                    self.updateLayerFeature(layer, feature, featureGeometry)
+        layerList = list(lines) + list(polygons)
+        for pointFeature in points.getFeatures():
+            pointGeometry = pointFeature.geometry()
+            for layer in layerList:
+                if not layer:
+                    continue
+                for feature in layer.getFeatures( self.getFeatureRequest(pointGeometry) ):
+                    featureGeometry = feature.geometry()
+                    vertices = list( featureGeometry.vertices() )
+                    for idx, vertice in enumerate(vertices):
+                        vertexGeom = core.QgsGeometry.fromPointXY( QgsPointXY(vertice) )
+                        if not pointGeometry.intersects(  vertexGeom  ):
+                            continue
+                        featureGeometry.deleteVertex( idx )
+                        if not featureGeometry.isValid():
+                            self.addSink(vertexGeom, sink_p, fields, layer.name())
+                            continue
+                        self.updateLayerFeature(layer, feature, featureGeometry)
+        return {self.OUTPUT_P: sinkId_p}
+
+    def addSink(self, geom, sink, fields, nameLayer):
+        newFeat = QgsFeature(fields)
+        newFeat.setGeometry(geom)
+        newFeat['erro'] = 'Não foi possível deleta o vértice ( {} )'.format(nameLayer)
+        sink.addFeature(newFeat)
+
+    def getFeatureRequest(self, geometry, distance=1, segment=5):
+        return QgsFeatureRequest().setFilterRect(
+            geometry.buffer(distance, segment).boundingBox()
+        )
 
     def updateLayerFeature(self, layer, feature, geometry):
         feature.setGeometry(geometry)
