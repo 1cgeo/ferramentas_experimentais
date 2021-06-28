2535808c0765412e09c3f5ec7b5ad6d50a195e7a
pedro-mar <pedroms410@gmail.com>
2021-06-28 15:56:54 -0300

    update streamOrder

--- processings/streamOrder.py
+++ processings/streamOrder.py
@@ -7,9 +7,10 @@ from qgis.core import (QgsProcessing,
                        QgsProcessingParameterFeatureSink,
                        QgsProcessingParameterVectorLayer,
                        QgsFeature,
-                       QgsField
+                       QgsField,
+                       QgsGeometry,
+                       QgsPointXY
                        )
-from qgis import processing
 
 class StreamOrder(QgsProcessingAlgorithm):
 
@@ -39,8 +40,8 @@ class StreamOrder(QgsProcessingAlgorithm):
         
         feedback.setProgressText('Verificando inconsistencias ')
         streamStartPoints = self.getLinesStartPoints(featuresArray)
-        orderedFeatures = self.orderLines(featuresArray, streamStartPoints, 0)
-        newLayer = self.orderedLayer(parameters, context, originalLayer, orderedFeatures)
+        self.orderLines(featuresArray, streamStartPoints, 0)
+        newLayer = self.orderedLayer(parameters, context, originalLayer, featuresArray)
 
         return {self.OUTPUT: newLayer}
 
@@ -55,49 +56,67 @@ class StreamOrder(QgsProcessingAlgorithm):
 
     def getLinesStartPoints(self, featuresArray):
         startPointsList = []
-
+        pointControl = []
         for feature in featuresArray:
             for geom in feature[0].geometry().constGet():
-                if geom[0] not in startPointsList:
-                    startPointsList.append(geom[0])
+                if geom[0] not in pointControl:
+                    startPointsList.append(QgsGeometry.fromPointXY(QgsPointXY(geom[0])))
+                    pointControl.append(geom[0])
 
         return startPointsList
 
     def orderLines(self, featuresArray, streamStartPoints, order=0):
-        newOrder = order+1
-        actualLine = self.nextLine(featuresArray, streamStartPoints)
-        
-        for feature in featuresArray:
-            if feature[0].attributes()[0] in actualLine:
-                feature[1]= newOrder
-                for geom in feature[0].geometry().constGet():
-                    if geom[0] in streamStartPoints:
-                        streamStartPoints.remove(geom[0])
-                    if not len(streamStartPoints):
-                        return featuresArray
+        if order ==0:
+            self.verticesIntersectOneRiver(featuresArray, streamStartPoints)
+            order +=1
+            self.orderLines(featuresArray, streamStartPoints, 1)
+        if len(streamStartPoints)==0:
+            return False
+        while (not len(streamStartPoints)==0):
+            self.verticesIntersectUnknownOrderRiver(featuresArray, streamStartPoints)
 
-        featuresArray=self.orderLines(featuresArray, streamStartPoints, newOrder)
-
-        return featuresArray
-        
-    def nextLine(self, featuresArray, streamStartPoints):
-        streamStartPointsCopy = streamStartPoints.copy()
-        newLineList = []
-        repeatedPoints = []
-
-        for feature in featuresArray:
-            attr = feature[-1]
-            for geom in feature[0].geometry().constGet():
-                if geom[-1] in streamStartPointsCopy  and (attr==0):
-                    repeatedPoints.append(geom[-1])
-            
-        [streamStartPointsCopy.remove(point) for point in repeatedPoints if point in streamStartPointsCopy]
-
-        for feature in featuresArray:
-                for geom in feature[0].geometry().constGet():
-                    if geom[0] in streamStartPointsCopy:
-                        newLineList.append(feature[0].attributes()[0])
-        return sorted(newLineList)
+        return False
+    def verticesIntersectOneRiver(self, featuresArray, streamStartPoints):
+        toBeRemoved = []
+        for point in streamStartPoints:
+            riversIntersected = []
+            for riverIndex in range(len(featuresArray)):
+                river  =featuresArray[riverIndex]
+                if point.intersects(river[0].geometry()):
+                    riversIntersected.append(riverIndex)
+            if len(riversIntersected)==1:
+                featuresArray[riversIntersected[0]][1]=1
+                toBeRemoved.append(point)
+        for pt in toBeRemoved:
+            streamStartPoints.remove(pt)
+        return False
+    def verticesIntersectUnknownOrderRiver(self, featuresArray, streamStartPoints):
+        toBeRemoved = []
+        for point in streamStartPoints:
+            maxOrder = 0
+            riversIntersected = []
+            riverUnknownOrderIntersected = []
+            intersectsEndOfUnknownRiver = False
+            for riverIndex in range(len(featuresArray)):
+                river  =featuresArray[riverIndex]
+                if point.intersects(river[0].geometry()):
+                    riversIntersected.append(riverIndex)
+                    if river[1]>maxOrder:
+                        maxOrder = river[1]
+                    for geom in river[0].geometry().constGet():
+                        ptIni = QgsGeometry.fromPointXY(QgsPointXY(geom[0]))
+                        ptFin = QgsGeometry.fromPointXY(QgsPointXY(geom[-1]))
+                    if river[1]==0 and point.intersects(ptFin):
+                        intersectsEndOfUnknownRiver = True
+                    if river[1]==0 and point.intersects(ptIni):
+                        riverUnknownOrderIntersected.append(riverIndex)
+            if (not len(riverUnknownOrderIntersected)==0) and (not len(riversIntersected)==len(riverUnknownOrderIntersected)) and (not intersectsEndOfUnknownRiver):
+                for riverUnkownOrder in riverUnknownOrderIntersected:
+                    featuresArray[riverUnkownOrder][1]=maxOrder+1
+                toBeRemoved.append(point)
+        for pt in toBeRemoved:
+            streamStartPoints.remove(pt)
+        return False
     
     def orderedLayer(self, parameters, context, originalLayer, orderedFeatures):
 
