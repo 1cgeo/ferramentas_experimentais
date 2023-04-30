022385d76791be592fb3ff256c54f49fcb31498e
Felipe Diniz <diniz.ime@gmail.com>
2023-04-30 09:45:41 -0300

    line2Multiline otimizado

--- processings/line2Multiline.py
+++ processings/line2Multiline.py
@@ -15,7 +15,8 @@ from qgis.core import (
                         QgsProcessingParameterNumber,
                         QgsGeometry,
                         QgsPointXY,
-                        QgsProcessingParameterFile
+                        QgsProcessingParameterFile,
+                        QgsLineString
                     )
 from qgis import processing
 from qgis import core, gui
@@ -58,35 +59,100 @@ class Line2Multiline(QgsProcessingAlgorithm):
             core.QgsWkbTypes.MultiLineString,
             QgsCoordinateReferenceSystem( iface.mapCanvas().mapSettings().destinationCrs().authid() )
         )
-        
 
+        lines = self.runAddCount(lines, feedback=feedback)
+        self.runCreateSpatialIndex(lines, feedback=feedback)
+        spatialJoinOutput = self.runSpatialJoin(lines, lines, feedback=feedback)
 
-        vl = core.QgsVectorLayer("MultiLineString", "spatialIndex", "memory")
-        vl.startEditing()
+        self.id_to_feature = {}
+        self.ids_in_stack = []
+        for currentFeature in lines.getFeatures():
+            self.id_to_feature[currentFeature['AUTO']] = currentFeature
+            self.ids_in_stack.append(currentFeature['AUTO'])
 
-        features = list(lines.getFeatures())
-        for currentFeature in features:
+        self.matching_features = {}
+        for feat in spatialJoinOutput.getFeatures():
+            if feat['AUTO'] not in self.matching_features:
+                self.matching_features[feat['AUTO']] = []
+            if feat['AUTO'] != feat['AUTO_2']:
+                	self.matching_features[feat['AUTO']].append(feat['AUTO_2'])
+
+        while len(self.ids_in_stack) > 0:
+            currentid = self.ids_in_stack[0]
+            del self.ids_in_stack[0]
+
+            mls_array = self.aggregate(currentid)
+
             mls = core.QgsMultiLineString()
-            mls.addGeometry( core.QgsLineString( list(currentFeature.geometry().vertices()) ) )
-            featuresRequest = list( vl.getFeatures( QgsFeatureRequest().setFilterRect( mls.calculateBoundingBox() ) ) )
-            for idx, currentFeature in enumerate(featuresRequest):
-                if core.QgsGeometry( mls.clone() ).intersects( currentFeature.geometry() ):
-                    for element in currentFeature.geometry().asGeometryCollection():
-                        mls.addGeometry( core.QgsLineString( list(element.vertices()) ) )
-                    vl.deleteFeature( currentFeature.id() )
-            feat = QgsFeature()
-            feat.setGeometry( core.QgsGeometry( mls.clone() ) )
-            vl.addFeatures( [ feat ] )
+            for el in mls_array:
+                mls.addGeometry( QgsLineString( list(el.vertices()) ) )
+            self.addSink( QgsGeometry( mls ), sink_l, fields)
 
-        for feat in vl.getFeatures():
-            geom = feat.geometry()
-            self.addSink( geom, geom.length(), sink_l, fields)
         return {self.OUTPUT_L: sinkId_l}
 
-    def addSink(self, geom, length, sink, fields):
+
+    def aggregate(self, featureId):
+        currentfeature = self.id_to_feature[featureId]
+        currentgeom = currentfeature.geometry()
+        mls_array = []
+        mls_array.append(currentgeom)
+
+        matching_features_id = [el for el in self.matching_features[featureId] if el in self.ids_in_stack]
+        
+        for match_id in matching_features_id:
+            self.ids_in_stack.remove(match_id)
+        
+        for match_id in matching_features_id:
+            agg_array = self.aggregate(match_id)
+            mls_array = mls_array + agg_array
+
+        return mls_array
+
+    def runSpatialJoin(self, streamLayerInput, countourLayer, feedback):
+        output = processing.run(
+            'native:joinattributesbylocation',
+            {
+                'INPUT': streamLayerInput,
+                'JOIN': countourLayer,
+                'PREDICATE': [0],
+                'JOIN_FIELDS': [],
+                'METHOD': 0,
+                'DISCARD_NONMATCHING': True,
+                'PREFIX': '',
+                'OUTPUT': 'TEMPORARY_OUTPUT' 
+            },
+            feedback=feedback
+        )
+        return output['OUTPUT']
+    
+    def runAddCount(self, inputLyr, feedback):
+        output = processing.run(
+            "native:addautoincrementalfield",
+            {
+                'INPUT':inputLyr,
+                'FIELD_NAME':'AUTO',
+                'START':0,
+                'GROUP_FIELDS':[],
+                'SORT_EXPRESSION':'',
+                'SORT_ASCENDING':False,
+                'SORT_NULLS_FIRST':False,
+                'OUTPUT':'TEMPORARY_OUTPUT'
+            },
+            feedback=feedback
+        )
+        return output['OUTPUT']
+
+    def runCreateSpatialIndex(self, inputLyr, feedback):
+        processing.run(
+            "native:createspatialindex",
+            {'INPUT':inputLyr},
+            feedback=feedback
+        )
+
+    def addSink(self, geom, sink, fields):
         newFeat = QgsFeature(fields)
         newFeat.setGeometry(geom)
-        newFeat['length'] = length
+        newFeat['length'] = geom.length()
         sink.addFeature(newFeat, QgsFeatureSink.FastInsert)
 
     def tr(self, string):
